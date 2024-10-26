2885a6afd8ca793f15bc1359143bbcc78b30fd4a
sakibsarker <sakib15-3824@diu.edu.bd>
2024-10-27 00:56:50 +0600

    done post properties

--- property/api.py
+++ property/api.py
@@ -1,7 +1,8 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view,authentication_classes,permission_classes
-
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from .forms import PropertyForm
 from .models import Property
 from .serializers import PropertiesListSerializer
 
@@ -15,3 +16,17 @@ def properties_list(request):
     return JsonResponse({
         'data':serializer.data
     })
+
+
+@api_view(['POST','FILES'])
+@permission_classes([IsAuthenticated])
+def create_property(request):
+    form=PropertyForm(request.POST,request.FILES)
+    if form.is_valid():
+        property=form.save(commit=False)
+        property.landlord=request.user
+        property.save()
+        return JsonResponse({'success':True})
+    else:
+        print('error',form.errors,form.non_field_errors)
+        return JsonResponse({'errors':form.errors.as_json()},status=400)

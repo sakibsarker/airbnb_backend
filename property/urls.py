43ef0a59ce9cac631c4b0b44e71b7f3459b354a7
sakibsarker <sakib15-3824@diu.edu.bd>
2024-10-30 03:10:54 +0600

    property booking api done

--- property/urls.py
+++ property/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',api.properties_list,name='api_properties_list'),
     path('<uuid:pk>/',api.properties_detail,name='api_property_detail'),
     path('create/',api.create_property,name='api_create_property'),
+    path('<uuid:pk>/book/',api.book_property,name='api_book_property'),
+
 ]
